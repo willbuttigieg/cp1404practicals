646a835ce903555045eecb6b042ce240da4ae042
William Buttigieg <williambuttigieg2005@gmail.com>
2025-09-23 10:39:43 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,18 @@
-password_length_requirement = 8
-password = str(input("Enter the password: "))
-while len(password) < password_length_requirement:
-    print(f"Password must be at least {password_length_requirement} characters long")
-    password = str(input("Enter the password: "))
-print("*" * len(password))
+def main():
+    password = get_valid_password()
+    print_password(password)
 
+
+def print_password(password: str):
+    print("*" * len(password))
+
+
+def get_valid_password() -> str:
+    password = str(input("Enter the password: "))
+    while len(password) < 8:
+        print("Password must be at least 8 characters long")
+        password = str(input("Enter the password: "))
+    return password
+
+
+main()
