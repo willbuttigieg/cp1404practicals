8b15deeedea5b80ec98529d81bd053806a52ade2
William Buttigieg <williambuttigieg2005@gmail.com>
2025-10-07 14:55:37 +1000

    complete inadequate password checker program

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,28 @@
+"""
+CP1404 Practical 4
+List Exercises
+"""
+
+# Basic list operations
+
+NUMBER_OF_NUMBERS_ENTERED = 5
+numbers = []
+
+for i in range(NUMBER_OF_NUMBERS_ENTERED):
+    number = int(input("Number: "))
+    numbers.append(number)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average number is {sum(numbers) / len(numbers)}")
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
