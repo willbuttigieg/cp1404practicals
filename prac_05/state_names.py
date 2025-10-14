41151a884a0312a227719139937f414cea26bc12
William Buttigieg <williambuttigieg2005@gmail.com>
2025-10-14 12:21:48 +1000

    Add exceptions and reformat to PEP8

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,15 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
-
-state_code = input("Enter short state: ")
+for key in CODE_TO_NAME:
+    print(f"{key:<3} is {CODE_TO_NAME[key]}")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
