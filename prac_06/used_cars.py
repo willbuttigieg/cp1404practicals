c62052e73241a5ca892b4ff78a6c17079def6d02
William Buttigieg <williambuttigieg2005@gmail.com>
2025-10-28 12:53:47 +1000

    Complete up to Q7 from Prac 6 Modifications

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -13,10 +13,10 @@ def main():
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
-    limo = Car(100)
+    limo = Car(100, "limo")
     limo.add_fuel(20)
     print(f"Car has fuel: {limo.fuel}")
     limo.drive(115)
-
+    print(limo)
 
 main()
