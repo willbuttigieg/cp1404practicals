4ac066e790720065643fedb06b0e8568215a6545
William Buttigieg <williambuttigieg2005@gmail.com>
2025-11-11 13:03:42 +1000

    Complete box layout demo exercise

---
+++ prac_08/KivyDemos-master/box_layout_demo.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        """Build BoxLayoutDemo app"""
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        """Handle greet button - print greeting with user input"""
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        """Handle clear button - clear greeting and user input"""
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
+
+
+BoxLayoutDemo().run()
